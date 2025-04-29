b1bf999c9f691485c1e8b01745c4f995b1038b46
tamim <tamimyousefi023@gmail.com>
2025-04-29 18:46:33 +0330

    button component improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Button from "@/components/Button/Button";
+import Input from "@/components/Input/Input";
 import ThemeProvider from "@/components/ThemeProvider";
 import { useEffect, useState } from "react";
 
@@ -12,22 +13,75 @@ export default function Home() {
     return () => clearTimeout(timeout);
   });
   return (
-    <main className="flex flex-col justify-center items-center h-screen gap-4">
+    <main className="flex flex-col justify-center items-center h-screen gap-14">
       <ThemeProvider />
-      <section className="flex flex-row gap-3.5">
-        <Button>Primary</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="disabled">Disabled</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button
-          onClick={() => setLoading(true)}
-          variant="secondary"
-          loading={loading}
-          className="w-32"
-        >
-          Loading
-        </Button>
+      <section className="flex flex-col space-y-5 w-full container mx-64 items-center  ">
+        <h1 className="text-2xl text-primary ">Button variants</h1>
+        <div className="flex flex-row flex-wrap gap-3.5 ">
+          <Button>Primary</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="disabled">Disabled</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button
+            onClick={() => setLoading(true)}
+            variant="secondary"
+            loading={loading}
+            className="w-32"
+          >
+            Loading
+          </Button>
+        </div>
+      </section>
+      <section className="flex flex-col space-y-5 w-full container mx-64 items-center">
+        <h1 className="text-2xl text-primary "> Input variants</h1>
+        <div className="flex flex-row gap-6 items-center">
+          {" "}
+          <div className="flex flex-col gap-4 items-center justify-center ">
+            <Input label="Default" placeholder="write here..." />
+            <Input label="Disabled" placeholder="write here..." disabled />
+            <Input label="Error" placeholder="write here..." error />
+          </div>
+          <div className="flex flex-col gap-4 items-center justify-center ">
+            <Input
+              label="Underlined"
+              variant="underlined"
+              placeholder="write here..."
+            />
+            <Input
+              label="Disabled"
+              variant="underlined"
+              placeholder="write here..."
+              disabled
+            />
+            <Input
+              label="Error"
+              variant="underlined"
+              placeholder="write here..."
+              error
+            />
+          </div>
+          <div className="flex flex-col gap-4 items-center justify-center ">
+            <Input
+              label="Ghost"
+              variant="ghost"
+              placeholder="write here..."
+              type="radio"
+            />
+            <Input
+              label="Disabled"
+              variant="ghost"
+              placeholder="write here..."
+              disabled
+            />
+            <Input
+              label="Error"
+              variant="ghost"
+              placeholder="write here..."
+              error
+            />
+          </div>
+        </div>
       </section>
     </main>
   );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,32 +2,48 @@ import React from "react";
 import LoadingSpinner from "../LoadingSpinner";
 
 const variants = {
-  primary: " bg-primary hover:brightness-120 border-none text-white ",
-  secondary: " text-primary border-primary hover:bg-secondary ",
-  disabled: " border-muted text-muted opacity-50 cursor-not-allowed",
-  ghost: " text-primary border-none  hover:bg-secondary",
-  destructive: " bg-red-600 hover:brightness-140 border-none text-white",
+  primary:
+    " bg-primary hover:brightness-120 border-none text-white  active:scale-[0.85] duration-200 ",
+  secondary:
+    " text-primary border-primary hover:bg-secondary  active:scale-[0.85]  duration-200 ",
+  disabled:
+    " border-muted text-muted-foreground opacity-50 cursor-not-allowed ",
+  ghost:
+    " text-primary border-none  hover:bg-secondary active:scale-[0.85]  duration-200 ",
+  destructive:
+    " bg-red-600 hover:brightness-140 border-none text-white active:scale-[0.85]  duration-200 ",
 };
-interface IButtonProps {
+const sizes = {
+  sm: "text-sm px-3 py-1.5",
+  md: "text-base px-4 py-2",
+  lg: "text-lg px-5 py-3",
+};
+
+interface ButtonProps {
   variant?: keyof typeof variants;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   loading?: boolean;
   className?: string;
   children: React.ReactNode;
+  size?: keyof typeof sizes;
+  type?: "submit" | "button";
 }
 
 const Button = ({
   variant = "primary",
+  type = "button",
   children,
   loading,
   className,
   onClick,
-}: IButtonProps) => {
+  size = "md",
+}: ButtonProps) => {
   return (
     <button
+      type={type}
       disabled={variant === "disabled" || loading}
       onClick={onClick}
-      className={` flex justify-center items-center transition-all  px-4 py-2 border-[2px] rounded-lg ${variants[variant]} ${className}`}
+      className={` flex justify-center items-center transition-all gap-2  border-[2px] rounded-lg ${variants[variant]} ${sizes[size]} ${className}`}
     >
       {loading ? <LoadingSpinner /> : children}
     </button>
